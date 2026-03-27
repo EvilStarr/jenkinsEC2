8373dee0ef38add5e60e24029fbd0db4f2e6163b
Jasper Shivers <jdollas2022@gmail.com>
2026-03-27 15:27:23 -0400

    six commit

--- 2-main.tf
+++ 2-main.tf
@@ -19,3 +19,39 @@ resource "aws_s3_object" "text" {
   key    = "armageddon_evidence/proof.txt"
   source = "${path.module}/armageddon_evidence/proof.txt"
 }
+
+resource "aws_s3_object" "jenkins1" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/jenkins1.png"
+  source = "${path.module}/jenkins_server_proof/jenkins1.png"
+}
+
+resource "aws_s3_object" "jenkins2" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/jenkins2.png"
+  source = "${path.module}/jenkins_server_proof/jenkins2.png"
+}
+
+resource "aws_s3_object" "jenkins3" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/jenkins3.png"
+  source = "${path.module}/jenkins_server_proof/jenkins3.png"
+}
+
+resource "aws_s3_object" "jenkins4" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/jenkins4.png"
+  source = "${path.module}/jenkins_server_proof/jenkins4.png"
+}
+
+resource "aws_s3_object" "s3proof" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/s3proof.png"
+  source = "${path.module}/jenkins_server_proof/s3proof.png"
+}
+
+resource "aws_s3_object" "webhook" {
+  bucket = aws_s3_bucket.frontend.id
+  key    = "jenkins_server_proof/webhook.png"
+  source = "${path.module}/jenkins_server_proof/webhook.png"
+}
