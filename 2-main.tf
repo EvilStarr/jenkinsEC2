fb556941240b9ccf50193db4222d6d9e2d6364cb
Jasper Shivers <jdollas2022@gmail.com>
2026-03-28 18:37:57 -0400

    seven commit

--- 2-main.tf
+++ 2-main.tf
@@ -55,3 +55,31 @@ resource "aws_s3_object" "webhook" {
   key    = "jenkins_server_proof/webhook.png"
   source = "${path.module}/jenkins_server_proof/webhook.png"
 }
+
+resource "aws_s3_bucket_public_access_block" "jdollasname" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "goku" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      },
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.pab_name]
+}
